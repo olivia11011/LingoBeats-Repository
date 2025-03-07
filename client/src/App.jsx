04b8d9247c78cdb718a9be8193b3aa6058e55bc4
jhachey718 <jhachey718@gmail.com>
2025-03-07 13:26:05 -0500

    Created login page (not connected to backend yet).

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 function App() {
-
   return (
-    <Routes>
-    <Route path="/" element={<Login />} />
-  </Routes>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Login() {
+  const [username, setUsername] = useState("");
   return (
-    <div className="flex min-h-screen justify-center items-center">
-      <h1>Login</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-indigo-600">
+      <div className="bg-gray-100 p-8 rounded-2xl shadow-lg w-96">
+        <h2 className="text-2xl font-bold text-center mb-4">
+          LingoBeats Login
+        </h2>
+        <form action="/login" method="POST" className="space-y-4">
+          <div>
+            <label className="block text-gray-800">Username</label>
+            <input
+              type="text"
+              name="username"
+              className=" bg-gray-100 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-gray-800">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="bg-gray-100 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+          >
+            Login
+          </button>
+        </form>
+        <div className="text-center mt-4">
+          <Link to="/signup" className="text-blue-500 hover:underline">
+            Sign up
+          </Link>
+          <span className="mx-2">|</span>
+          <Link to="/dashboard" className="text-blue-500 hover:underline">
+            Continue as Guest
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
